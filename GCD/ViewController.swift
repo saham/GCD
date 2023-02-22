c60c329e8df6f423707fe64628b4c5f893aea925
Sammy Ghazavi <saham.ghazavi@gmail.com>
2023-02-21 21:40:07 -0500

    Initial detup

---
+++ GCD/ViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import BigInt // For big integers
+class ViewController: UIViewController {
+    var workItem: DispatchWorkItem?
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var answerTextView: UITextView!
+    @IBAction func calculate(_ sender: UIButton) {
+        var result:BigInt = BigInt(1)
+        calculateButton.isEnabled = false
+        let newWorkItem = DispatchWorkItem {
+            self.workItem?.cancel()
+            result = self.LongRunningFunction()
+        }
+        let notifyView = DispatchWorkItem {
+            self.UpdateUI( result: result)
+        }
+        newWorkItem.notify(queue: .main) {
+            notifyView.perform()
+        }
+        self.workItem = newWorkItem
+        DispatchQueue.global().async(execute: newWorkItem)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func LongRunningFunction() -> BigInt {
+        return (1 ... BigInt(5555)).map { BigInt($0) }.reduce(BigInt(1), *)
+    }
+    func UpdateUI(result: BigInt) {
+        print(result)
+        self.calculateButton.isEnabled = true
+        self.answerTextView.text = "\(result)"
+    }
+}
+
+
